9e06607172c07add2c9fcb6f30be2326e101dd61
Alejo <alejojpa2@gmail.com>
2024-09-25 09:45:56 -0400

    State initialization

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
+    const [todos, setTodos] = useState([]);
+    const [headingInput, setHeadingInput] = useState('');
+    const [listInputs, setListInputs] = useState({});
   
-
-  return (
+    return (
     <>
       <div className="todo-container">
         <h1 className="title">My Todo List</h1>
