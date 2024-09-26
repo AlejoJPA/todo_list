b5716fc2df66db643e3416f1bf3e98d9603e0609
Alejo <alejojpa2@gmail.com>
2024-09-26 06:38:50 -0400

    Add Heading Functionality for Todo List and Display it

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -5,6 +5,13 @@ const TodoList = () => {
     const [todos, setTodos] = useState([]);
     const [headingInput, setHeadingInput] = useState('');
     const [listInputs, setListInputs] = useState({});
+
+    const handleAddTodo = () => {
+        if (headingInput.trim() !== '') {
+        setTodos([...todos, { heading: headingInput, lists: [] }]);
+        setHeadingInput('');
+        }
+    };
   
     return (
     <>
@@ -15,16 +22,25 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value);}} 
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
+
       <div className="todo_main">
-        
+        {todos.map((todo, index) => (
+            <div key={index} className="todo-card">
+                <div className="heading_todo">
+                    <h3>{todo.heading}</h3> {/* Display the heading here */}
+                    <button className="delete-button-heading">Delete Heading </button>
+                </div>
+            </div>
+        ))}        
       </div>
     </>
-  );
+    );
 };
 
 export default TodoList;
