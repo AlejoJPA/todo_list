bb8260c208935c94fb8789c4097d0b4750ee7bfe
Alejo <alejojpa2@gmail.com>
2024-10-14 11:36:28 +0200

    Creating handleAddTodo, handleDeleteTodo and their outputs (render)

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,7 +2,27 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([]); // Initializes todos. Empty array indicates that there are not to-do items initially
+  const [headingInput, setHeadingInput] = useState(''); // Initialize headingInput. Empty string intended for adding new to-do headers 
+  const [listInputs, setListInputs] = useState({});  //Initialize listInputs as an empty object {}. This state will hold the value of input fields for each to-do item.
 
+  //This function (handleAddTodo) gets the heading from the input box with className="heading-input". It is triggered by onClick={handleAddTodo},
+  //JS trim() method is used to remove whitespace from both ends of a string.
+  /*spread syntax (...) in JavaScript allows you to "spread" elements from an array (e.g., ...todos), object, or iterable into individual elements or properties.*/
+  /*The display of the Todo heading is made by the function in div className="todo_main"*/
+
+  const handleAddTodo = () => { 
+    if (headingInput.trim() !== '') {
+      setTodos([...todos, {heading: headingInput, list: []}]);
+      setHeadingInput('');
+    }
+  };
+
+  const handleDeleteTodo = (index) =>{
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <>
@@ -13,13 +33,22 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value);}}
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
 
-      <div className="todo_main"> // section intended to show the TO-DO Items
+      <div className="todo_main">
+        {todos.map((todo, index) => (
+          <div key={index} className= "todo-card">
+            <div className="heading_todo">
+              <h3>{todo.heading}</h3>
+              <button className="delete-button-heding" onClick={() => handleDeleteTodo(index)}>Delete Heading</button>
+            </div>
+          </div>
+        ))}
         
       </div>
     </>
